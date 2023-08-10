20267c2aa5e728ff81adac0ab1d170611e1b6aac
Volkov Alexander <volkov.525@gmail.com>
2023-08-10 18:37:44 +0300

    add simple tree

---
+++ src/components/BaseTree.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const nodes = [
+	{
+		label: 'Satisfied customers (with avatar)',
+		avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		children: [
+			{
+				label: 'Good food (with icon)',
+				icon: 'mdi-silverware',
+				children: [
+					{ label: 'Quality ingredients' },
+					{ label: 'Good recipe' }
+				]
+			},
+			{
+				label: 'Good service (disabled node with icon)',
+				icon: 'mdi-silverware',
+				disabled: true,
+				children: [
+					{ label: 'Prompt attention' },
+					{ label: 'Professional waiter' }
+				]
+			},
+			{
+				label: 'Pleasant surroundings (with icon)',
+				icon: 'mdi-silverware',
+				children: [
+					{
+						label: 'Happy atmosphere (with image)',
+						img: 'https://cdn.quasar.dev/img/logo_calendar_128px.png'
+					},
+					{ label: 'Good table presentation' },
+					{ label: 'Pleasing decor' }
+				]
+			}
+		]
+	}
+]
+</script>
+
+<template lang="pug">
+q-tree(:nodes="nodes" node-key="label" default-expand-all)
+</template>
+
+<style scoped lang="scss"></style>
--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
+import BaseTree from '@/components/BaseTree.vue'
 
 const splitterModel = ref(30)
 const insideModel = ref(50)
@@ -7,43 +8,6 @@ const insideModel = ref(50)
 const hei = computed(() => {
 	return 'height: ' + (window.innerHeight - 170) + 'px;'
 })
-const simple = [
-	{
-		label: 'Satisfied customers (with avatar)',
-		avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
-		children: [
-			{
-				label: 'Good food (with icon)',
-				icon: 'restaurant_menu',
-				children: [
-					{ label: 'Quality ingredients' },
-					{ label: 'Good recipe' }
-				]
-			},
-			{
-				label: 'Good service (disabled node with icon)',
-				icon: 'room_service',
-				disabled: true,
-				children: [
-					{ label: 'Prompt attention' },
-					{ label: 'Professional waiter' }
-				]
-			},
-			{
-				label: 'Pleasant surroundings (with icon)',
-				icon: 'photo',
-				children: [
-					{
-						label: 'Happy atmosphere (with image)',
-						img: 'https://cdn.quasar.dev/img/logo_calendar_128px.png'
-					},
-					{ label: 'Good table presentation' },
-					{ label: 'Pleasing decor' }
-				]
-			}
-		]
-	}
-]
 </script>
 
 <template lang="pug">
@@ -53,8 +17,7 @@ q-page(padding)
 		q-splitter(v-model="splitterModel" :limits="[0, 100]" :style="hei" )
 			template(v-slot:before)
 				q-scroll-area.list
-					p laksldjal alsj 
-					// qdraggabletree(:nodes="simple")
+					BaseTree
 			template(v-slot:after)
 				q-splitter(v-model="insideModel" horizontal)
 					template(v-slot:before)
