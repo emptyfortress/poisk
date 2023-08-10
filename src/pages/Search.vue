b035079a0cf8f319ff460d7ce138a663ed9b328c
Volkov Alexander <volkov.525@gmail.com>
2023-08-10 17:19:08 +0300

    remove drag tree

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -7,6 +7,43 @@ const insideModel = ref(50)
 const hei = computed(() => {
 	return 'height: ' + (window.innerHeight - 170) + 'px;'
 })
+const simple = [
+	{
+		label: 'Satisfied customers (with avatar)',
+		avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		children: [
+			{
+				label: 'Good food (with icon)',
+				icon: 'restaurant_menu',
+				children: [
+					{ label: 'Quality ingredients' },
+					{ label: 'Good recipe' }
+				]
+			},
+			{
+				label: 'Good service (disabled node with icon)',
+				icon: 'room_service',
+				disabled: true,
+				children: [
+					{ label: 'Prompt attention' },
+					{ label: 'Professional waiter' }
+				]
+			},
+			{
+				label: 'Pleasant surroundings (with icon)',
+				icon: 'photo',
+				children: [
+					{
+						label: 'Happy atmosphere (with image)',
+						img: 'https://cdn.quasar.dev/img/logo_calendar_128px.png'
+					},
+					{ label: 'Good table presentation' },
+					{ label: 'Pleasing decor' }
+				]
+			}
+		]
+	}
+]
 </script>
 
 <template lang="pug">
@@ -17,6 +54,7 @@ q-page(padding)
 			template(v-slot:before)
 				q-scroll-area.list
 					p laksldjal alsj 
+					// qdraggabletree(:nodes="simple")
 			template(v-slot:after)
 				q-splitter(v-model="insideModel" horizontal)
 					template(v-slot:before)
