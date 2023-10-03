8caeac36c823008d83c3d5354e139268aafd9a3f
Volkov Alexander <volkov.525@gmail.com>
2023-10-03 13:42:58 +0300

    fix grids

--- src/components/poiskDialog.vue
+++ src/components/poiskDialog.vue
@@ -26,54 +26,53 @@ q-dialog(v-model="dialog")
 	q-card.dialog
 		q-card-section.row.items-center.q-pb-none
 			q-btn(flat round dense @click="switchSidebar" )
-				q-icon(name="mdi-forwardburger" v-if="sidebar")
-				q-icon(name="mdi-backburger" v-else)
+				q-icon(name="mdi-backburger" v-if="sidebar")
+				q-icon(name="mdi-forwardburger" v-else)
 			.text-h6.q-ml-lg Поиск 1
 			q-space
 			q-btn(icon="mdi-close" flat round dense v-close-popup )
 
 		q-separator
-		.topgrid
-			q-scroll-area.list
+		.topgrid(:class="{ side: sidebar }")
+			q-scroll-area.list(v-if="sidebar")
 				BaseTree(:treeData="mySearches")
 			q-form.grid
 				template(v-for="el in mystore.attributes" :key="el.id")
 					formItem(:item="el" wind )
-				p(v-for="n in 40") ljkalsj lakj l
 
-		q-card-actions.q-mt-md(align="between")
+		q-card-actions.q-pa-md(align="between")
 			q-btn(flat color="primary" label="Отмена" v-close-popup) 
 			.q-gutter-md
 				q-btn(flat color="primary" icon="mdi-cog" label="Настроить" @click="goToEdit") 
 				q-btn(unelevated color="primary" icon="mdi-magnify" label="Искать" v-close-popup) 
 
-// EditSearch(v-model="edit" )
+EditSearch(v-model="edit" )
 </template>
 
 <style scoped lang="scss">
 .dialog {
 	width: 780px;
 	max-width: 90vw;
-	// min-height: 300px;
 }
 
 .topgrid {
+	width: 100%;
 	display: grid;
-	grid-template-columns: auto 1fr;
+	grid-template-columns: 1fr;
 	grid-template-rows: minmax(250px, 1fr);
-	// justify-items: start;
 	align-items: start;
-	column-gap: 2rem;
-	// background: pink;
-	margin-right: 1rem;
-	margin-top: 1rem;
+	column-gap: 1rem;
+	padding: 1rem;
+
+	&.side {
+		grid-template-columns: auto 1fr;
+	}
 }
 
 .list {
-	// height: minmax(250px, 600px);
 	height: 100%;
 	font-size: .9rem;
-	background: #ededed;
+	background: var(--bg-panel);
 	border-right: 1px solid #ccc;
 	width: 200px;
 }
