a923670f4eeea79d14065c41763a8ffa2d1c8019
Volkov Alexander <volkov.525@gmail.com>
2024-01-15 15:29:39 +0300

    add draw page

--- src/components/DrawBottom.vue
+++ src/components/DrawBottom.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import draggable from 'vuedraggable'
+
+const list = ref([
+	{ name: 'one', id: 0 },
+	{ name: 'two', id: 1 },
+])
+</script>
 
 <template lang="pug">
-.text-center Что здесь?
+draggable(:list="list" class="list-group" group="a" item-key="name")
+	template(#item="{ element }")
+		.list-group-item {{ element.name }}
+
+	template(#header)
+		.list-group-item
+			q-btn(unelevated color="primary" label="Отмена" @click="action") 
 </template>
 
 <style scoped lang="scss"></style>
