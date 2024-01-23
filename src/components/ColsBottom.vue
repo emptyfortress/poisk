b270e04b757ecbb1677e812269aab417a3decc41
Volkov Alexander <volkov.525@gmail.com>
2024-01-23 14:16:18 +0300

    finish drag

--- src/components/ColsBottom.vue
+++ src/components/ColsBottom.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { uid } from 'quasar'
 import draggable from 'vuedraggable'
 import { datasource, options } from '@/stores/twolist'
 import FieldTree from '@/components/FieldTree.vue'
+import { useDrag } from '@/stores/drag'
 
-const list = ref([...datasource])
+const drag = useDrag()
+const list = ref([])
+
+const end = (ev: Event) => {
+	ev.preventDefault()
+}
+const insert = () => {
+	if (drag.dragNode) {
+		let temp = {
+			id: uid(),
+			check: true,
+			sort: true,
+			label: drag.dragNode.text,
+		}
+		list.value.push(temp)
+	}
+}
+const remove = (ind: number) => {
+	list.value.splice(ind, 1)
+}
 </script>
 
 <template lang="pug">
@@ -17,14 +38,18 @@ q-scroll-area(style="height: 40vh;")
 			.text-weight-bold Колонки:
 			draggable(:list="list"
 				item-key="id"
+				tag="ul"
 				class="list-group"
 				ghost-class="ghost"
+				:ondragover="end"
+				:ondrop="insert"
 				group="data")
-				template(#item="{ element }")
-					.list-group-item
+
+				template(#item="{ element, index }")
+					li.list-group-item
 						.dragg
 						q-checkbox.q-mr-lg(v-model="element.check" dense :label="element.label")
-						q-btn.right(flat round icon="mdi-close" dense size="xs") 
+						q-btn.right(flat round icon="mdi-close" dense size="xs" @click="remove(index)") 
 		.sort
 			.text-weight-bold Сортировка:
 			div Тип &uarr;
@@ -37,9 +62,8 @@ q-scroll-area(style="height: 40vh;")
 	margin: 0 auto;
 	display: grid;
 	grid-template-columns: 1fr 1fr 1fr;
-	justify-items: start;
 	align-items: start;
-	column-gap: 1rem;
+	column-gap: 2rem;
 	row-gap: 0.5rem;
 	font-size: 0.9rem;
 }
@@ -49,7 +73,6 @@ q-scroll-area(style="height: 40vh;")
 	cursor: move;
 }
 .list-group-item {
-	width: 100%;
 	display: flex;
 	justify-content: space-between;
 	background: #fff;
@@ -63,13 +86,6 @@ q-scroll-area(style="height: 40vh;")
 	font-size: 0.9rem;
 	font-weight: 600;
 	margin-bottom: 0.5rem;
-}
-.drag {
-	display: grid;
-	grid-template-columns: 230px 1fr;
-	gap: 2rem;
-	// max-height: 450px;
-	// background: pink;
 }
 .dragg {
 	position: absolute;
@@ -89,5 +105,25 @@ q-scroll-area(style="height: 40vh;")
 	justify-items: start;
 	align-items: start;
 	gap: 1rem;
+}
+ul,
+li {
+	margin: 0;
+	padding: 0;
+	list-style: none;
+}
+ul {
+	margin-top: 1rem;
+}
+ul:empty:after {
+	display: block;
+	content: 'Пусто. Настройте колонки, перетащив их из дерева слева.';
+	width: 100%;
+	text-align: center;
+	color: rgba(0, 0, 0, 0.5);
+	height: 75px;
+	// line-height: 75px;
+	border-radius: 4px;
+	font-size: 0.9rem;
 }
 </style>
