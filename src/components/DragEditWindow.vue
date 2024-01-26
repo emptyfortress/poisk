7fee1aa935162abc22838fb18020af752366d638
Volkov Alexander <volkov.525@gmail.com>
2024-01-26 16:41:25 +0300

    cool

--- src/components/DragEditWindow.vue
+++ src/components/DragEditWindow.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { UseDraggable as Draggable } from '@vueuse/components'
+import ColumnSetup from '@/components/ColumnSetup.vue'
 
 const modelValue = defineModel()
 const el = ref<HTMLElement | null>(null)
@@ -17,12 +18,13 @@ transition(name="slide-top")
 		style="position: fixed;"
 		:handle="handle")
 		q-card-section.row.items-center.q-pb-none(ref="handle")
-			.text-h6 Hello
+			.text-h6 Настройка представления
 			q-space
-			q-btn(icon="mdi-close" flat round dense @click="modelValue = false")
+			q-btn(unelevated color="primary" label="Применить" @click="apply" size="sm")
+			q-btn.q-ml-lg(icon="mdi-close" flat round dense @click="modelValue = false")
 
 		q-card-section
-			p Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
+			ColumnSetup(revert)
 </template>
 
 <style scoped lang="scss">
@@ -31,8 +33,8 @@ transition(name="slide-top")
 	resize: both;
 }
 .fuck {
-	width: 500px;
-	height: 300px;
+	width: 900px;
+	height: 400px;
 	background: white;
 	box-shadow: var(--card-shadow);
 	resize: both;
