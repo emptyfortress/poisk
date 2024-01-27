be477d83443785feabbda399efbc28a85af43d93
Volkov Alexander <volkov.525@gmail.com>
2024-01-27 11:38:14 +0300

    fix placement

--- src/components/DragEditWindow.vue
+++ src/components/DragEditWindow.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { UseDraggable as Draggable } from '@vueuse/components'
 import ColumnSetup from '@/components/ColumnSetup.vue'
 
 const modelValue = defineModel()
 const el = ref<HTMLElement | null>(null)
 const handle = ref<HTMLElement | null>(null)
+const initial = computed(() => {
+	let xval = window.innerWidth / 2 - 300
+	let yval = window.innerHeight / 2
+	return { x: xval, y: yval }
+})
 </script>
 
 <template lang="pug">
@@ -13,14 +18,14 @@ transition(name="slide-top")
 	Draggable.fuck(v-slot="{ x, y }"
 		:prevent-default="true"
 		v-if="modelValue"
-		:initial-value="{ x: 340, y: 240 }"
+		:initial-value="initial"
 		prevent-default="true"
 		style="position: fixed;"
 		:handle="handle")
-		q-card-section.row.items-center.q-pb-none(ref="handle")
-			.text-h6 Настройка представления
+		q-card-section.sec(ref="handle")
+			.text-h6 Представление 1
 			q-space
-			q-btn(unelevated color="primary" label="Применить" @click="apply" size="sm")
+			q-btn(unelevated color="primary" label="Применить" size="sm")
 			q-btn.q-ml-lg(icon="mdi-close" flat round dense @click="modelValue = false")
 
 		q-card-section
@@ -31,6 +36,13 @@ transition(name="slide-top")
 .q-card {
 	width: 500px;
 	resize: both;
+}
+.sec {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	cursor: move;
+	padding-bottom: 0;
 }
 .fuck {
 	width: 900px;
