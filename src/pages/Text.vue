8cd99e84ac4bafce1dc3152b7442209bf96cfddc
Volkov Alexander <volkov.525@gmail.com>
2024-02-19 14:33:24 +0300

    before refactor to flat list

--- src/pages/Text.vue
+++ src/pages/Text.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import { options as stringOptions } from '@/stores/options'
-
-const keys = computed(() => {
-	return options.value.filter((e) => e.selected)
-})
+import { getMembers } from '@/utils/utils'
 
 const options = ref(stringOptions)
+const flatList = computed(() => {
+	return getMembers(options.value)
+})
+
+// const keys = computed(() => {
+// 	return flatList.value.filter((e) => e.selected)
+// })
+const keys = computed({
+	get() {
+		return flatList.value.filter((e) => e.selected)
+	},
+	set(newValue) {},
+})
 
 const filterFn = (val: string, update: Function) => {
 	update(() => {
@@ -25,7 +35,9 @@ const reset = () => {
 	options.value.map((e) => (e.selected = false))
 }
 const remove = (e: any) => {
-	e.opt.selected = false
+	console.log(e.opt.id)
+	let idx = flatList.value.findIndex((item) => item.id == e.opt.id)
+	console.log(idx)
 }
 </script>
 
@@ -33,7 +45,6 @@ const remove = (e: any) => {
 q-page(padding)
 	.container
 		.zag.q-mb-lg Простой поиск
-		// pre {{ keys }}
 		q-select(dense
 			v-model="keys"
 			use-input
@@ -53,7 +64,7 @@ q-page(padding)
 			template(v-slot:option="scope")
 			template(v-slot:no-option)
 			template(v-slot:selected-item="scope")
-				q-chip(v-model="scope.selected" removable @remove="remove(scope)") {{scope.opt.text}}
+				q-chip(v-model="scope.selected" dense removable @remove="remove(scope)") {{scope.opt.text}}
 		.grid
 			q-list
 				q-item(v-for="item in options" :key="item.id" clickable @click="add(item)" :class="{selected: item.selected}")
@@ -61,13 +72,14 @@ q-page(padding)
 						q-item-label {{item.text}}
 			transition(name="slide-right")
 				q-list(v-if="keys.length > 0")
+					q-item(v-for="item in options[0].children" :key="item.id" clickable @click="add(item)" :class="{selected: item.selected}")
+						q-item-section
+							q-item-label {{item.text}}
+			transition(name="slide-right")
+				q-list(v-if="keys.length > 1")
 					q-item
 						q-item-section
 							q-item-label fuck
-			q-list(v-if="keys.length > 1")
-				q-item
-					q-item-section
-						q-item-label fuck
 </template>
 
 <style scoped lang="scss">
@@ -76,24 +88,16 @@ q-page(padding)
 }
 .grid {
 	display: flex;
-	gap: 1rem;
+	gap: 2rem;
 }
-// .grid {
-// 	display: grid;
-// 	grid-template-columns: repeat(3, 1fr);
-// 	justify-items: start;
-// 	align-items: start;
-// 	column-gap: 1rem;
-// 	row-gap: 0.5rem;
-// 	font-size: 0.9rem;
-// 	.q-list {
-// 		background: pink;
-// 	}
-// }
 .selected {
 	background: $blue-2;
 }
 .q-item {
 	margin-bottom: 1px;
+}
+.q-chip {
+	margin-bottom: 0;
+	margin-top: 0;
 }
 </style>
